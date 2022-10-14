2d7b469546251496afcfba9bd4bb12b05d40a369
anna <anjasudol@gmail.com>
2022-10-14 17:37:19 +0100

    functions

--- contracts/Weth.sol
+++ contracts/Weth.sol
@@ -17,8 +17,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract WETH {
     ERC20 private WETHToken;
 
-    event Deposit(address indexed account, uint amount);
-    event Withdraw(address indexed account, uint amount);
+    event Stake(address indexed stakeholder, uint256 amount);
+    event Unstake(address indexed stakeholder, uint256 amount);
      struct Stakeholder {
         uint256 amount;
     }
@@ -36,6 +36,7 @@ contract WETH {
         require(WETHToken.balanceOf(msg.sender) >= value, "Staking value is higher that your account balance");
         stakeholders[msg.sender].amount += value;
         WETHToken.transferFrom(msg.sender, address(this), value);
+        emit Stake(msg.sender, value);
     }
 
     /**
@@ -45,6 +46,26 @@ contract WETH {
         uint256 rewards = stakeholders[msg.sender].amount;
         stakeholders[msg.sender].amount = 0;
         WETHToken.transfer(msg.sender, rewards);
+        emit Unstake(msg.sender, rewards);
+
+    }
+
+    /**
+     * withdraws LP tokens available for withdrawal.
+     */
+    function unstake(uint256 value) external {
+        uint256 rewards = stakeholders[msg.sender].amount;
+
+        require(rewards >= value, "Unstaked value is higher that staking amount");
+        stakeholders[msg.sender].amount -=value;
+        WETHToken.transfer(msg.sender, value);
+        emit Unstake(msg.sender, value);
+
+    }
+
+    function checkSatkingBalance(address staking_address) public view returns (uint256) {
+         uint256 rewards = stakeholders[staking_address].amount;
+         return rewards;
     }
 
 
