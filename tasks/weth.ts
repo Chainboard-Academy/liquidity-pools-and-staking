2f16b744c35eb4b9d6c9991d2172e03477505fe5
anna <anjasudol@gmail.com>
2022-10-14 20:25:57 +0100

    task

--- tasks/weth.ts
+++ tasks/weth.ts
@@ -7,11 +7,23 @@ dotenv.config();
 
 let deployedContract: any;
 const contract_name = "WETH";
-const ERC20_CONTRACT_ADDRESS: string = process.env.ERC20_CONTRACT_ADDRESS as string;
-
+// const ERC20_CONTRACT_ADDRESS: string = process.env.ERC20_CONTRACT_ADDRESS as string;
+const ERC20_CONTRACT_ADDRESS = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 task("accounts", "Prints the list of accounts").setAction(async (taskArgs, hre) => {
     const accounts = await hre.ethers.getSigners();
     for (const account of accounts) {
         console.log(account.address);
     }
 });
+
+task("stake", "transfers LP tokes from the user to the contract.")
+    .addParam("amount", "The amount to transfer")
+    .setAction(async (taskArgs, hre) => {
+        const account = taskArgs.account;
+        const contract = await hre.ethers.getContractAt('WETH', ERC20_CONTRACT_ADDRESS);
+        const amount = hre.ethers.utils.parseUnits(taskArgs.amount, await contract.decimals());
+        const signer = await hre.ethers.getSigners();
+        console.log("stacking amount ", amount);
+        let tx = await contract.connect(signer[0]).stake(amount);
+        console.log('stacking transaction was successful: ', tx.hash);
+    });
