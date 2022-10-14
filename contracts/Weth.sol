bfb3e49f41ad3d0836000f04f281313d7663205d
anna <anjasudol@gmail.com>
2022-10-14 16:09:04 +0100

    weth

--- contracts/Weth.sol
+++ contracts/Weth.sol
@@ -3,6 +3,23 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WETH is ERC20 {
-    constructor() ERC20('Wrapped Ether', 'WETH');
+    event Deposit(address indexed account, uint amount);
+    event Withdraw(address indexed account, uint amount);
+    constructor() ERC20('Wrapped Ether', 'WETH') {};
+    fallback() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        _mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint _amount) external payable {
+        _mint(msg.sender, msg.value);
+        payable(msg.sender).transfer(_amount);
+        emit Withdraw(msg.sender, msg.value);
+    }
+
 
 }
