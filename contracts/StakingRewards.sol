df4460db8bb572080da7cd655ba2316a44bdc8bf
anna <anjasudol@gmail.com>
2022-10-20 23:37:55 +0100

    staking

--- contracts/StakingRewards.sol
+++ contracts/StakingRewards.sol
@@ -6,14 +6,16 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract StakingRewards is AccessControl {
-    uint256 public minStakingDays = 7;
+    uint256 public minStakingDays = 2;
+    uint public rewardsRate;
     IERC20 public immutable stakingToken;
     ERC20 public immutable rewardsToken;
 
     constructor(address _stakingToken, address _rewardsToken) {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        stakingToken=IERC20(_stakingToken);
-        rewardsToken=ERC20(_rewardsToken);
+        stakingToken=IERC20(_stakingToken); //ERC20
+        rewardsToken=ERC20(_rewardsToken);//LP
+        rewardsRate = 10;
     }
 
     struct Stakeholder {
@@ -27,12 +29,23 @@ contract StakingRewards is AccessControl {
     event Stake(address indexed staker, uint256 amount);
     event Unstake(address indexed staker, uint256 amount);
 
+     modifier updateReward() {
+        uint256 reward_updated = _calculateRewards(msg.sender);
+        stakeholders[msg.sender].rewards = reward_updated;
+        rewardsToken.increaseAllowance(msg.sender, reward_updated);
+        _;
+    }
+
     function getStakeHoldersStake(address stakeHolder) external view returns (uint256) {
         return stakeholders[stakeHolder].amount;
     }
 
     function getStakeHoldersRewards(address stakeHolder) external view returns (uint256) {
         return stakeholders[stakeHolder].rewards;
+    }
+
+    function changeRewardRate(uint256 newRate) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        rewardsRate = newRate;
     }
 
     //transfers LP tokes from the user to the contract. 
@@ -42,14 +55,44 @@ contract StakingRewards is AccessControl {
         stakeholders[msg.sender].amount += stakedAmount;
         stakeholders[msg.sender].stakingTime += block.timestamp;
 
-        stakingToken.transferFrom(msg.sender, address(this), stakedAmount);
+        stakingToken.transferFrom(msg.sender, address(this), stakedAmount); //transfer amount from ERC20 contract to this WETH contract
         emit Stake(msg.sender, stakedAmount);
+        return true;
+    }
+    //withdraws LP tokens available for withdrawal.
+    function unstake() updateReward external returns(bool) {
+        uint256 rewards_available = stakeholders[msg.sender].amount;
+        stakeholders[msg.sender].amount - rewards_available;
+        rewardsToken.transfer(msg.sender, rewards_available);
+        rewardsToken.decreaseAllowance(msg.sender, rewards_available);
         return true;
     }
 
     //withdraws reward tokens available to the user from the contract
-    function claim() public {}
+    function claim(uint256 _amount) updateReward external returns(bool) {
+        uint256 rewards_available = stakeholders[msg.sender].amount;
+        require(_amount >= rewards_available, "No enough funds to withdraw");
+        stakeholders[msg.sender].amount - _amount;
+        rewardsToken.transfer(msg.sender, _amount);
+        rewardsToken.decreaseAllowance(msg.sender, _amount);
+        return true;
+    }
 
-    //withdraws LP tokens available for withdrawal.
-    function unstake() public {}
+    //----internal functions----
+    function _calculateRewards(address stakeholder) internal returns(uint) {
+        uint256 stakedTime =_getStakeHoldersStakingTime(stakeholder);
+        uint256 time = block.timestamp - stakedTime;
+        uint units = time / 1 minutes;
+        uint updatedRewards;
+        for (uint256 i = 0; i < units; i++){
+            updatedRewards = stakeholders[stakeholder].amount / rewardsRate;
+            stakeholders[stakeholder].rewards += updatedRewards;
+        }
+        return updatedRewards;
+
+    }
+
+    function _getStakeHoldersStakingTime(address stakeHolder) internal view returns (uint256) {
+        return stakeholders[stakeHolder].stakingTime;
+    }
 }
