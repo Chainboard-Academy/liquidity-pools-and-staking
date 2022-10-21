fc9a46abd97cd9de0db86144f46f6fce610d7140
anna <anjasudol@gmail.com>
2022-10-21 18:03:05 +0100

    test

--- test/StakingRewards.test.ts
+++ test/StakingRewards.test.ts
@@ -58,17 +58,12 @@ describe("StakingRewards", function () {
         it('reverts transaction, due to not enough funds', async () => {
             const balance = await erc20_token.balanceOf(account1.address);
             const tx = staking_rewards_token.connect(account1).stake((balance + 1));
-            await expect(tx).to.be.revertedWith("Not enough funds");
+            await expect(tx).to.be.revertedWith("Sender balance is too low");
         });
         describe('Unstaking', function () {
-            let initial_balance: any;
-            beforeEach(async function () {
+            it('complete transaction', async function () {
                 await erc20_token.connect(account1).approve(staking_rewards_token.address, 10);
                 await staking_rewards_token.connect(account1).stake(10);
-                initial_balance = await erc20_token.balanceOf(account1.address);
-                console.log(initial_balance)
-            });
-            it('complete transaction', async function () {
                 const initial_staking_balance = await staking_rewards_token.getStakeHoldersStakedAmount(account1.address);
                 await erc20_token.connect(account1).approve(staking_rewards_token.address, 10);
                 const tx = staking_rewards_token.connect(account1).unstake();
@@ -78,11 +73,7 @@ describe("StakingRewards", function () {
                 // const staking_balance = await staking_rewards_token.getStakeHoldersStakedAmount(account1.address);
                 // expect(staking_balance).to.be.equal(0);
             });
-            it('reverts transaction, due to not enough funds', async () => {
-                const balance = await erc20_token.balanceOf(account1.address);
-                const tx = staking_rewards_token.connect(account1).stake((balance + 1));
-                await expect(tx).to.be.revertedWith("Sender balance is too low");
-            });
+
         });
     });
 });
