e05f44c09c8f366cdfff55a5182cfd97bd428b06
anna <anjasudol@gmail.com>
2022-10-22 19:36:44 +0100

    tasks

--- tasks/staking_rewards.ts
+++ tasks/staking_rewards.ts
@@ -10,7 +10,7 @@ const REWARD_TOKEN_ADDRESS: string = process.env.LP_CONTRACT_ADDRESS || '';
 
 task("stake", "Deposit tokens to ERC20")
     .addParam("amount", "amount to stake")
-    .setAction(async (taskArgs: { amount: any; account: any }, hre) => {
+    .setAction(async (taskArgs: { amount: any }, hre) => {
         const staking_token = await hre.ethers.getContractAt("StakingRewards", STAKING_CONTRACT_ADDRESS);
         const rewards_token = await hre.ethers.getContractAt("ERCStandard20", REWARD_TOKEN_ADDRESS);
         const account = await hre.ethers.getSigners();
@@ -22,7 +22,7 @@ task("stake", "Deposit tokens to ERC20")
 
 task("unstake", "withdraws tokens")
     .addParam("amount", "amount to unstake")
-    .setAction(async (taskArgs: { amount: any; account: any }, hre) => {
+    .setAction(async (taskArgs: { amount: any }, hre) => {
         const staking_token = await hre.ethers.getContractAt("StakingRewards", STAKING_CONTRACT_ADDRESS);
         const account = await hre.ethers.getSigners();
 
@@ -31,7 +31,7 @@ task("unstake", "withdraws tokens")
     });
 
 task("claim", "withdraws all of the available rewards")
-    .setAction(async (taskArgs: { amount: any; account: any }, hre) => {
+    .setAction(async (hre) => {
         const staking_token = await hre.ethers.getContractAt("StakingRewards", STAKING_CONTRACT_ADDRESS);
         const account = await hre.ethers.getSigners();
         const tx = await staking_token.connect(account[0]).claim();
