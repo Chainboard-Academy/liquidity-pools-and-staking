816ffca07bff87adae4f2b49caf16941ecb5914c
Anna Sudol <anjasudol@gmail.com>
2022-11-03 15:37:34 +0000

    test

--- scripts/deploy_staking_rewards.ts
+++ scripts/deploy_staking_rewards.ts
@@ -3,14 +3,14 @@ const LP_CONTRACT_ADDRESS: string = process.env.LP_CONTRACT_ADDRESS || ''; //Liq
 const ERC20_CONTRACT_ADDRESS: string = process.env.ERC20_CONTRACT_ADDRESS || ''; //ERC20 contract used for staking
 
 async function main() {
-    console.log("Deploying StakingRewards contract");
-    const StakingRewards = await ethers.getContractFactory("Staking");
+    console.log("Deploying Staking contract");
+    const Staking = await ethers.getContractFactory("Staking");
     console.log(LP_CONTRACT_ADDRESS, ERC20_CONTRACT_ADDRESS, 'LP_CONTRACT_ADDRESS, ERC20_CONTRACT_ADDRESS')
-    const staking_rewards_token = await StakingRewards.deploy(LP_CONTRACT_ADDRESS, ERC20_CONTRACT_ADDRESS);
-    console.log("StakingRewards contract deployed to:", staking_rewards_token.address);
+    const staking_rewards_token = await Staking.deploy(LP_CONTRACT_ADDRESS, ERC20_CONTRACT_ADDRESS);
+    console.log("Staking contract deployed to:", staking_rewards_token.address);
 
     await staking_rewards_token.deployed();
-    console.log("StakingRewards contract deployed to2:", staking_rewards_token.address);
+    console.log("Staking contract deployed to2:", staking_rewards_token.address);
 }
 
 main()
